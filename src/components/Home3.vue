849ed8ed2d0deae52dfc5710017d84813dd0014e
MMoonBird <mmoonbird@hotmail.com>
2019-06-26 17:19:09 +0800

    父子组件主动获取对方的数据和方法<理论>

--- src/components/Home3.vue
+++ src/components/Home3.vue
@@ -5,15 +5,7 @@
       首页组件
     </div>
 </template>
-<!--
-  父组件给子组件传值
-    1,在父组件调用子组件时为它绑定一个动态属性
-    <v-header :title="title"></v-header>
-    2,在子组件中接收父组件传递的数据
-      在子组件中，定义一个prop属性。
-      props可以是数组或对象
-      props:['title']
--->
+
 <script>
   import header from './Header2'
     export default {
---
+++ src/components/Home4.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <v-header ref="header"></v-header>
+  </div>
+
+
+</template>
+<!--
+  父组件主动获取子组件的数据和方法
+    1,调用子组件的时候定义ref
+      <v-header ref="header"></v-header>
+     2,在父组件中使用
+     this.$refs.header.属性
+     this.$refs.header.方法
+     属性就可以获取
+  子组件主动获取父组件的数据和方法
+-->
+<script>
+  import header from './Header3'
+    export default {
+        name: "Home4",
+      components:{
+          "v-header":header
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
